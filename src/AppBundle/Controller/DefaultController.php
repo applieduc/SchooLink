ada9d23a3e0468b594e55755442da86956e9c40e
Digicixgroup <digicixgroup@gmail.com>
2018-12-15 14:49:19 +0100

    dfjgjk

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('template/layout.html.twig');
+    
+        return $this->redirect('/login');
     }
 }
